79c8afa40c91dca677d0331623c6be948a4f6c5a
Le Nguyen Ngoc <lebkp04@gmail.com>
2024-02-16 17:22:16 +0700

    first commit - simple UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// import react-router-dom
+import {
+	BrowserRouter as Router,
+	Route,
+	Routes,
+} from "react-router-dom";
+
+//import pages
+import Home from '../src/pages/Home';
+import ProductDetails from '../src/pages/ProductDetails';
+
+//import component
+import SideBar from '../src/components/Sidebar';
+import Header from '../src/components/Header';
+import Footer from '../src/components/Footer';
+
+const App = () => {
+	return (
+		<div className="overflow-hidden">
+			<Router>
+				<Header />
+				<Routes>
+					<Route path="/" element={<Home />}/>
+					<Route path="/product/:id" element={<ProductDetails />} />
+				</Routes>
+				<SideBar />
+				<Footer />
+			</Router>
+		</div>
+	);
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//product provider
+import ProductProvider from './contexts/ProductContext';
+
+//sidebar provider
+import SidebarProvider from './contexts/SidebarContext';
+
+//cart provider
+import CartProvider from './contexts/CartContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<SidebarProvider>
+		<CartProvider>
+			<ProductProvider>
+				<React.StrictMode>
+					<App />
+				</React.StrictMode>
+			</ProductProvider>
+		</CartProvider>
+	</SidebarProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
